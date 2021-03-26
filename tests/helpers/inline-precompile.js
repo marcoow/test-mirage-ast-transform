d2f4ad4f0f0da76396e7f5f912112f7d399748f4
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2021-03-26 15:44:18 +0100

    make it work

--- tests/helpers/inline-precompile.js
+++
@@ -1,5 +0,0 @@
-import { hbs } from 'ember-cli-htmlbars';
-
-export default function inlinePrecompile(string) {
-  return hbs(string);
-}
--- tests/integration/cool-test.js
+++ tests/integration/cool-test.js
@@ -1,7 +1,7 @@
 import { module, test } from "qunit";
 import { setupRenderingTest } from "ember-qunit";
 import { render } from "@ember/test-helpers";
-import { hbs } from '../helpers/inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 
 module("cool", function (hooks) {
     setupRenderingTest(hooks);
